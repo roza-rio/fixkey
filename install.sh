3dd0def3db2e5250e4000b64f09cc7ce70f0c112
Daniel Rozario <61970262+roza-rio@users.noreply.github.com>
2021-03-03 11:06:57 -0500

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo apt install figlet
+sudo wget -O /bin/fixkey https://raw.githubusercontent.com/roza-rio/fixkey/main/fixkey && sudo chmod +x /bin/fixkey
+figlet -t "Done"
+rm install.sh
